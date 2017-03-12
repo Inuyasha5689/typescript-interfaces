a0314cdc2b8fd9da6b9be8480c36265442b9e230
Drake Scott <dascott0204@gmail.com>
2017-03-12 04:14:58 -0500

    interfaces!!

--- app.ts
+++ app.ts
@@ -1,3 +1,23 @@
 /**
  * Created by dasco on 3/9/2017.
  */
+interface NamedPerson {
+    firstName: string;
+}
+
+function greet(person: NamedPerson) {
+    console.log("Hello, " +person.firstName);
+}
+
+function changeName(person: NamedPerson) {
+    person.firstName = "Anna";
+}
+
+const person = {
+    firstName: "drake",
+    age: 20
+};
+
+greet(person);
+changeName(person);
+greet(person);
