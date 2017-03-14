04197a352d41efe6926927f3958218a6ebee2fb8
Drake Scott <dascott0204@gmail.com>
2017-03-13 19:36:04 -0500

    interface inheritance

--- app.ts
+++ app.ts
@@ -55,3 +55,19 @@ myDoubleFunction = function (value1: number, value2: number) {
 };
 
 console.log(myDoubleFunction(10,20));
+
+// Interface Inheritance
+
+interface AgedPerson extends NamedPerson {
+    age: number;
+}
+
+const oldPerson: AgedPerson = {
+    age: 20,
+    firstName: "Drake",
+    greet(lastName: string){
+        console.log("hello!");
+    }
+};
+
+console.log(oldPerson);
