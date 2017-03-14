673494008c624a6fa5bd296b668d19d04c73ee86
Drake Scott <dascott0204@gmail.com>
2017-03-13 19:24:21 -0500

    function types with interfaces

--- app.ts
+++ app.ts
@@ -42,3 +42,16 @@ myPerson.firstName = "Alexander";
 myPerson.lastName = "Scott";
 greet(myPerson);
 myPerson.greet(myPerson.lastName);
+
+// Function Types
+
+interface DoubleValueFunc {
+    (number1: number, number2: number): number;
+}
+
+let myDoubleFunction: DoubleValueFunc;
+myDoubleFunction = function (value1: number, value2: number) {
+    return (value1 + value2) * 2;
+};
+
+console.log(myDoubleFunction(10,20));
